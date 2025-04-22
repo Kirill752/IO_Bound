6b406652b6ad5462759d76790e21a94532c0cb40
Kirill Latyshov <kil.real.latexshow@gmail.com>
2025-04-22 19:44:26 +0300

    Добавил README

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+// Обработчик, который работает долгое время
 type PdfHandler struct {
 	t string
 }
@@ -21,7 +22,7 @@ func (pdf *PdfHandler) Type() string {
 
 func (pdf *PdfHandler) Handle(ctx context.Context, t *task.Task) error {
 	log.Printf("task with ID: %s started", t.ID)
-	time.Sleep(1 * time.Minute)
+	time.Sleep(3 * time.Minute)
 	log.Printf("task with ID: %s finished", t.ID)
 	return nil
 }
@@ -32,32 +33,9 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// strg.Delete(ctx, "pdfHandler:test content")
-	// strg.GetAll(ctx)
-	// // strg.Save(ctx, t.GetKey(), &t)
-	// data, err := strg.Get(ctx, t.GetKey())
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// fmt.Println(string(data))
 	wp := workerpool.New(strg, 5)
 	wp.NewHandler(&PdfHandler{"pdfHandler"})
 	wp.Start(ctx)
-	// for i := 0; i < 10; i++ {
-	// 	// t := task.Task{
-	// 	// 	ID:       uuid.New().String(),
-	// 	// 	Type:     "pdfHandler",
-	// 	// 	Status:   task.Pending,
-	// 	// 	Input:    strconv.Itoa(i),
-	// 	// 	CreateAt: time.Now(),
-	// 	// 	UpdateAt: time.Now(),
-	// 	// }
-	// 	// wp.Add(&t)
-	// strg.Delete(ctx, fmt.Sprintf("pdfHandler:%s", "pdfHandler:test content"))
-	// }
-	// wp.Stop()
-	// str, _ := strg.Get(ctx, "pdfHandler:test content")
-	// fmt.Println(string(str))
 	serv := server.New(strg, wp)
 	serv.SetupGracefulShutdown()
 	mux := http.NewServeMux()
@@ -75,7 +53,6 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	// Запуск сервера
 	log.Println("Starting server on :8080")
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("Server error: %v", err)
